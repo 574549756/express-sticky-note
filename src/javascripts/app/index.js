ecca9f4e5dea519e15d01544be29cd281d82ff13
574549756 <574549756@qq.com>
2018-08-08 00:21:42 +0800

    note前端部分

--- src/javascripts/app/index.js
+++ src/javascripts/app/index.js
@@ -1,3 +1,15 @@
-var Toast = require('../mod/toast.js').Toast
+require('less/index.less')
 
-Toast('hello world')
+var Event = require('../mod/event.js')
+var WaterFall = require('../mod/waterfall.js')
+var NoteManager = require('../mod/note-manager.js').NoteManager
+
+NoteManager.load()
+
+$('.add-note').on('click', function() {
+    NoteManager.add()
+})
+
+Event.on('waterfall', function() {
+    WaterFall.init($('#content'))
+})
--- src/javascripts/mod/a.js
+++
@@ -1,6 +0,0 @@
-module.exports.a = 'bbbbbb'
-
-module.exports = {
-    b: 'bbbbbb',
-    a: a
-}
