2b7aabf243a1775cd863e1d0aa6cdf9a7ef8f43d
574549756 <574549756@qq.com>
2018-08-08 18:49:59 +0800

    页面

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,9 @@ var router = express.Router()
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-    res.render('index', { title: 'Express' })
+    console.log(req.query)
+
+    res.render('index', { title: 'My Note' })
 })
 
 module.exports = router
--- src/webpack.config.js
+++ src/webpack.config.js
@@ -1,30 +1,49 @@
 var webpack = require('webpack')
 var path = require('path')
+var ExtractTextPlugin = require('extract-text-webpack-plugin')
+var autoprefixer = require('autoprefixer')
 
 module.exports = {
-    entry: path.join(__dirname, 'javascripts/app/index.js'),
+    entry: path.join(__dirname, "js/app/index.js"),
     output: {
-        path: path.join(__dirname, '../public/javascripts'),
-        filename: 'index.js'
+        path: path.join(__dirname, "../public"),
+        filename: "js/index.js"
+    },
+    module: {
+        rules: [{
+            test: /\.less$/,
+            use: ExtractTextPlugin.extract({
+                    fallback: "style-loader",
+                    use: ["css-loader", "less-loader", "postcss-loader"]
+                }) //把 css 抽离出来生成一个文件
+        }]
     },
     resolve: {
         alias: {
-            jquery: path.join(__dirname, 'javascripts/lib/jquery-3.3.1.min.js'),
-            mod: path.join(__dirname, 'js/mod'),
-            less: path.join(__dirname, 'less')
+            jquery: path.join(__dirname, "js/lib/jquery-2.0.3.min.js"),
+            mod: path.join(__dirname, "js/mod"),
+            less: path.join(__dirname, "less")
         }
-    },
-    module: {
-        rules: [
-            {
-                test: /\.less$/,
-                use: ['style-loader', 'css-loader', 'less-loader']
-            }
-        ]
     },
     plugins: [
         new webpack.ProvidePlugin({
-            $: 'jquery'
+            $: "jquery"
+        }),
+        new ExtractTextPlugin("css/index.css"),
+        new webpack.LoaderOptionsPlugin({
+            options: {
+                postcss: [
+                    autoprefixer(),
+                ]
+            }
         })
+        // new webpack.optimize.UglifyJsPlugin({
+        //     compress: {
+        //         warnings: false,
+        //     },
+        //     output: {
+        //         comments: false,
+        //     },
+        // }),
     ]
-}
+};
