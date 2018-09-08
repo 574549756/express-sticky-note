2bfb87d8e5ad665e59c8d00403e06944e4a957c1
574549756 <574549756@qq.com>
2018-09-08 22:43:40 +0800

    github login

--- routes/auth.js
+++ routes/auth.js
@@ -14,9 +14,19 @@ passport.deserializeUser(function(id, done) {
 })
 
 /* GET auth . */
-router.get('/github', function(req, res, next) {
-    console.log(req.query)
-    res.render('index', { title: '我的note' })
-})
+passport.use(
+    new GitHubStrategy(
+        {
+            clientID: GITHUB_CLIENT_ID,
+            clientSecret: GITHUB_CLIENT_SECRET,
+            callbackURL: 'http://127.0.0.1:3000/auth/github/callback'
+        },
+        function(accessToken, refreshToken, profile, cb) {
+            User.findOrCreate({ githubId: profile.id }, function(err, user) {
+                return cb(err, user)
+            })
+        }
+    )
+)
 
 module.exports = router
